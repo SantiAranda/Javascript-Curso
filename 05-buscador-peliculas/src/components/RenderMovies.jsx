1a62bca19ca96276b021df457511980e05d2d92b
santiaranda <santiago.aranda.81@gmail.com>
2025-08-29 12:19:07 -0300

    shopping cart first commit

--- 05-buscador-peliculas/src/components/RenderMovies.jsx
+++ 05-buscador-peliculas/src/components/RenderMovies.jsx
@@ -1,26 +1,22 @@
-function ListOfMovies ({ movies }) {
+function ListOfMovies({ movies }) {
   return (
     <ul className="mainContent">
-      {
-        movies.map(movie => (
-          <li  key={movie.id}>
-            <img src={movie.poster} alt={movie.title} />
-            <h3>{movie.title}</h3>
-            <p>{movie.year}</p>
-          </li>
-        ))
-      }
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <img src={movie.poster} alt={movie.title} />
+          <h3>{movie.title}</h3>
+          <p>{movie.year}</p>
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
 function NoResults() {
-  return <p>No se ha encontrado peliculas para esta busqueda...</p>
+  return <p>No se ha encontrado peliculas para esta busqueda...</p>;
 }
 
 export function RenderMovies({ movies }) {
-  const hasMovies = movies?.length > 0
-  return (
-    hasMovies ? <ListOfMovies movies={movies} /> : <NoResults />
-  )
+  const hasMovies = movies?.length > 0;
+  return hasMovies ? <ListOfMovies movies={movies} /> : <NoResults />;
 }
--- 05-buscador-peliculas/src/hooks/useSearch.jsx
+++ 05-buscador-peliculas/src/hooks/useSearch.jsx
@@ -1,29 +1,28 @@
-import {useEffect, useState, useRef} from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function useSearch() {
-  const [query, setQuery] = useState("")
-  const [error, setError] = useState(null)
-  const isFirstInput = useRef(true)
+  const [query, setQuery] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
     if (isFirstInput.current) {
-      isFirstInput.current = query === ''
-      return
+      isFirstInput.current = query === "";
+      return;
     }
 
     if (query === "") {
-      setError("No se puede buscar una pelicula vacia")
-      return
+      setError("No se puede buscar una pelicula vacia");
+      return;
     }
 
     if (query.length < 3) {
-      setError("No se puede buscar una pelicula con menos de 3 caracteres")
-      return
+      setError("No se puede buscar una pelicula con menos de 3 caracteres");
+      return;
     }
 
-    setError(null)
-
+    setError(null);
   }, [query]);
 
-  return {query, setQuery, error}
+  return { query, setQuery, error };
 }
